dd25939d4d87eed073f3a726d54c98c42aa1da6e
The Dad <Shihen1611@gmail.com>
2021-02-25 21:24:06 -0600

    first commit

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var answer = 4;
+            //if (answer < 9)
+            //{
+            //    Console.WriteLine($"{answer} is less than 9");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{answer} is greater than or equal to 9");
+            //}
+
+
+
+            var answer = 4;
+            
+
+           var response = (answer < 9) ? "{answer} is  less than nine" : "{answer} is equal to or greater than 9";
+            Console.WriteLine( response);
+           
+
+                Console.WriteLine("Hello World!");
+        }
+    }
+}
